e579ce42f6cf83ad469d0c39e5867d6a38d29960
elroyschool <ebenjamins01@student.rocvantwente.nl>
2019-03-06 11:01:19 +0100

    Opdracht 8

--- Sprint9/Mp3Struct.cs
+++ Sprint9/Mp3Struct.cs
@@ -9,16 +9,16 @@ namespace MP3Constructor
     {
 
         public int Id;
-        /*public int Stock;*/
+        public int Stock;
         public string Make;
         public string Model;
         public float Mb;
         public double Price;
 
-        public MP3(int p_id, /*int p_stock,*/ string p_make, string p_model, float p_mb, double p_price)
+        public MP3(int p_id, int p_stock, string p_make, string p_model, float p_mb, double p_price)
         {
             this.Id = p_id;
-            /*this.Stock = p_stock;*/
+            this.Stock = p_stock;
             this.Make = p_make;
             this.Model = p_model;
             this.Mb = p_mb;
--- Sprint9/Program.cs
+++ Sprint9/Program.cs
@@ -1,46 +1,32 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sprint9
+using MP3Constructor;
+
+namespace Program
 {
     class Program
     {
+        public static List<MP3> MediaPlayers = new List<MP3>();
+
         static void Main(string[] args)
         {
-            // LogIn();
-
-
-
-            Mp3 mp;
-
-            mp.ID = 1;
-            mp.Brand = "GET technologies .inc";
-            mp.Model = "HF 410";
-            mp.MBSize = 4096;
-            mp.Price = 129.95;
-
-            Console.WriteLine(mp);
-
+            LogIn();
             Console.ReadLine();
         }
 
-        struct Mp3
-        {
-            public int ID;
-            public string Brand;
-            public string Model;
-            public int MBSize;
-            public double Price;
-        };
+
 
         private static void ShowMenu()
         {
             Console.WriteLine("");
             Console.WriteLine("Welk menu nummer?");
             Console.WriteLine("Overzicht mp3 spelers (1) ");
+            Console.WriteLine("Vooraad mp3 spelers (2) ");
             Console.WriteLine("Exit (9)");
 
             ConsoleKey key;
@@ -51,19 +37,30 @@ namespace Sprint9
             {
                 case ConsoleKey.D1:
                     {
-                        Console.WriteLine("gekozen actie: 1.Overzicht mp3 spelers");
+                        MediaPlayerList();
+                        ShowMp3s();
+                        break;
+                    }
+                case ConsoleKey.D2:
+                    {
+                        MediaPlayerList();
+                        ShowStock();
+                        break;
+                    }
+                case ConsoleKey.D8:
+                    {
+                        ShowMenu();
                         break;
                     }
                 case ConsoleKey.D9:
                     break;
-
                 default:
                     {
                         break;
                     }
-
             }
         }
+
         private static void LogIn()
         {
 
@@ -76,24 +73,48 @@ namespace Sprint9
             do
             {
                 wachtwoord = Console.ReadLine();
+                x++;
                 if (wachtwoord == "e")
                 {
                     Console.WriteLine("Welkom bij SoundSharp " + naam);
                     ShowMenu();
                     break;
                 }
-                else if (x <= 2)
+                else if (x == 2)
                 {
                     Console.WriteLine("Poging " + x + " van de 3.");
-                    x++;
                 }
                 else if (x == 3)
                 {
                     Console.WriteLine("Let op laatste poging!");
-                    x++;
                 }
             } while (x != 4);
 
+        }
+
+        static void ShowStock()
+        {
+            foreach (MP3 mpData in MediaPlayers)
+            {
+                Console.WriteLine("ID {0}, stock {1}", mpData.Id, mpData.Stock);
+            }
+        }
+
+        private static void ShowMp3s()
+        {
+            foreach (MP3 mpData in MediaPlayers)
+            {
+                Console.WriteLine("ID: {0} Make: {1, -14} Model: {2, -12} Capacity: {3, -6} Price: {4, -8} Stock: {5}", mpData.Id, mpData.Make, mpData.Model, mpData.Mb, mpData.Price, mpData.Stock);
+            }
+        }
+
+        public static void MediaPlayerList()
+        {
+            MediaPlayers.Add(new MP3(1, 500, "GetTech inc.", "HF 410", 4096, 129.99));
+            MediaPlayers.Add(new MP3(2, 500, "Far & Loud", "XM 600", 8192, 224.95));
+            MediaPlayers.Add(new MP3(3, 500, "Innotivative", "Z3", 512, 79.95));
+            MediaPlayers.Add(new MP3(4, 500, "Resistance", "3001", 4096, 124.95));
+            MediaPlayers.Add(new MP3(5, 500, "CBA", "NXT Volume", 2048, 159.05));
         }
     }
 }
